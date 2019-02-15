8e1532fef9a24de6d7e800ddc8bafe638751b742
qfstudy <qifeistudy@gmail.com>
2019-02-15 17:04:09 +0800

    添加grid组件

---
+++ docs/.vuepress/components/Grid/vi-grid.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="vi-input">
+      <vi-row class="row">
+        <vi-col class="col" :span="24">
+          <div class="grid-content bg-purple-dark">gfg</div>
+        </vi-col>
+      </vi-row>
+      <vi-row class="row"  :gutter="20">
+        <vi-col class="col" :span="12">
+          <div class="grid-content bg-purple">dd</div>
+        </vi-col>
+        <vi-col class="col" :span="12">
+          <div class="grid-content bg-purple-light">s</div>
+        </vi-col>
+      </vi-row>
+    </div>
+</template>
+
+<script>
+import Col from '../../../../src/packages/grid/src/col'
+import Row from '../../../../src/packages/grid/src/row'
+export default {
+  components:{
+    'vi-col': Col,
+    'vi-row': Row
+  }
+}
+</script>
+<style scoped>
+  hr{
+    border:none;
+  }
+</style>
